793e404fa7d6758740faf8328641182a17a3c0d5
Полина Дорошенко <malinpolin@gmail.com>
2022-09-22 10:20:59 +0300

    add entrypoint

--- api_yamdb/entrypoint.sh
+++ api_yamdb/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-PYTHONPATH=/app:$PYTHONPATH python
 python manage.py migrate --noinput && \
 python manage.py collectstatic --no-input && \
 gunicorn api_yamdb.wsgi:application --bind 0:8000
