fe36a8621c764fb947909552f6dc42a58ef14069
Полина Дорошенко <malinpolin@gmail.com>
2022-09-23 15:01:37 +0300

    test createsuperuser

--- api_yamdb/entrypoint.sh
+++ api_yamdb/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput && \
 python manage.py collectstatic --no-input && \
-# source '/app/.env' && python manage.py createsuperuser --no-input && \
+python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_USERNAME --password $DJANGO_SUPERUSER_PASSWORD && \
 gunicorn api_yamdb.wsgi:application --bind 0:8000
