5e53ed29b660103185e2451bf316ea96694e3df9
Полина Дорошенко <malinpolin@gmail.com>
2022-09-23 12:40:05 +0300

    add createsuperuser

--- api_yamdb/entrypoint.sh
+++ api_yamdb/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput && \
 python manage.py collectstatic --no-input && \
-python manage.py createsuperuser --noinput && \
+django-admin createsuperuser --no-input && \
 gunicorn api_yamdb.wsgi:application --bind 0:8000
