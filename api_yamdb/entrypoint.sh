f7adf8da2f0ba15d512b0fc4c8ac50c560ecf973
Полина Дорошенко <malinpolin@gmail.com>
2022-09-23 13:47:53 +0300

    delete createsuperuser

--- api_yamdb/entrypoint.sh
+++ api_yamdb/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput && \
 python manage.py collectstatic --no-input && \
-source './infra/.env' && python manage.py createsuperuser --no-input && \
+# source '/app/.env' && python manage.py createsuperuser --no-input && \
 gunicorn api_yamdb.wsgi:application --bind 0:8000
