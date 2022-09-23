bfbdede26eba6271da9731b09b852ceb8d5bd861
Полина Дорошенко <malinpolin@gmail.com>
2022-09-23 12:06:36 +0300

    static

--- api_yamdb/entrypoint.sh
+++ api_yamdb/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput && \
 python manage.py collectstatic --no-input && \
+python manage.py createsuperuser --noinput && \
 gunicorn api_yamdb.wsgi:application --bind 0:8000
