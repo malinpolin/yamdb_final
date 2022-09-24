b8da12073062c4c5a5674e7f80cebd8aa54a26e6
Полина Дорошенко <malinpolin@gmail.com>
2022-09-24 12:58:55 +0300

    try createsuperuser

--- api_yamdb/entrypoint.sh
+++ api_yamdb/entrypoint.sh
@@ -3,6 +3,9 @@ python manage.py migrate --noinput && \
 python manage.py collectstatic --no-input && \
 if [ "$DJANGO_SUPERUSER_USERNAME" ]
 then
-    python manage.py createsuperuser --noinput
+    python manage.py createsuperuser --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL \
+        --password $DJANGO_SUPERUSER_PASSWORD
 fi
 gunicorn api_yamdb.wsgi:application --bind 0:8000
